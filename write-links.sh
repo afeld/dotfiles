ce3e59e56c2b376bf62299bfb9899bc962d242a5
Aidan Feldman <aidan@jux.com>
2011-02-25 19:14:01 +0000

    added script for writing links to dotfiles

---
+++ write-links.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+# based on https://github.com/ahri/dotfiles/blob/master/write-links.sh
+script="`readlink -f "$0"`"
+dir="`dirname "$script"`"
+
+find "$dir" -maxdepth 1 | while read file; do
+
+  case "$file" in
+    "$dir"|"$dir/.git"|"$dir/.gitignore"|"$dir/README"|*.swp|"$script")
+      continue
+      ;;
+  esac
+  
+  name=".`basename $file`"
+  rm -rf "$HOME/$name"
+  ln -s "$file" "$HOME/$name"
+done

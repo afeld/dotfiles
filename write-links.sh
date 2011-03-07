aacd2e09d661d3e8ddb14d88b7d976a68c64e56e
Aidan Feldman <aidan@jux.com>
2011-03-07 13:25:55 -0500

    use python realpath instead of readlink for mac compatibility

--- write-links.sh
+++ write-links.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # based on https://github.com/ahri/dotfiles/blob/master/write-links.sh
-script="`readlink -f "$0"`"
+script="`python -c 'import os,sys;print os.path.realpath(sys.argv[1])' "$0"`" #"`readlink -f "$0"`"
 dir="`dirname "$script"`"
 
 find "$dir" -maxdepth 1 | while read file; do

bddfef470f142a89727d9ebaf95767c404e9adf2
Aidan Feldman <aidan.feldman@gmail.com>
2012-01-19 01:37:57 -0500

    removed sublime text default & pristine packages

--- sublime-text-2/Packages/Default/save_on_focus_lost.py
+++
@@ -1,7 +0,0 @@
-import sublime, sublime_plugin
-
-class SaveOnFocusLost(sublime_plugin.EventListener):
-    def on_deactivated(self, view):
-        if (view.file_name() and view.is_dirty() and
-                view.settings().get('save_on_focus_lost') == True):
-            view.run_command('save');

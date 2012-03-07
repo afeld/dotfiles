6f0c2f43a34127b11ea409d1fb7d3ed267e53e43
Aidan Feldman <aidan.feldman@gmail.com>
2012-03-07 00:54:40 -0500

    added rvm shell script

--- bash_profile_includes/rvm.sh
+++ bash_profile_includes/rvm.sh
@@ -1,2 +1,5 @@
 source ${HOME}/.rvm/scripts/rvm;
 export CC=/usr/bin/gcc-4.2
+
+[[ -s "/Users/aidan/.rvm/scripts/rvm" ]] && source "/Users/aidan/.rvm/scripts/rvm" # Load RVM into a shell session *as a function*
+

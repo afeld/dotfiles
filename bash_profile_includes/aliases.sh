4d54a9c394c026ba28be97ee1b1f5b286ed7d194
Aidan Feldman <aidan.feldman@gmail.com>
2012-10-04 18:28:23 -0400

    diff full-width

--- bash_profile_includes/aliases.sh
+++ bash_profile_includes/aliases.sh
@@ -3,6 +3,8 @@ alias rbp="git pull --rebase && git push"
 alias zshconfig="subl ~/.zshrc"
 alias ohmyzsh="subl ~/.oh-my-zsh"
 alias dot="cd ~/dotfiles && subl ."
+# diff full-width, c/o http://unix.stackexchange.com/a/9303
+alias diff='diff -W $(( $(tput cols) - 2 ))'
 
 # Jux-specific
 alias jux="cd ~/dev/surround/ && git pull && open surround.sublime-project"

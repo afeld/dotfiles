6d15c746f3e235d22c63908cfd8e9bc8b9953ee4
Aidan Feldman <aidan.feldman@gmail.com>
2012-11-29 14:21:20 -0500

    fix for jruby debugging

--- bash_profile_includes/rvm.sh
+++ bash_profile_includes/rvm.sh
@@ -1,3 +1,6 @@
 # export CC=/usr/bin/gcc-4.2
 
+# without this, JRuby treats 'next' as 'step' - http://kenai.com/projects/jruby/pages/UsingTheJRubyDebugger
+export JRUBY_OPTS="$JRUBY_OPTS --debug"
+
 [[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" # Load RVM into a shell session *as a function*

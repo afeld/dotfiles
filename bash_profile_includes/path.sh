bd01c3954bd43014e34edf93e9938f6828d27fbd
Aidan Feldman <aidan.feldman@gmail.com>
2012-12-17 16:18:02 -0500

    add npm bin folder to PATH

--- bash_profile_includes/path.sh
+++ bash_profile_includes/path.sh
@@ -1,5 +1,6 @@
 export PATH=$HOME/bin:$PATH
 export PATH=./node_modules/.bin:$PATH # add NodeJS modules from working directory
+export PATH=/usr/local/share/npm/bin:$PATH
 export PATH=/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/X11/bin:$PATH
 export PATH=$HOME/.rvm/bin:$PATH # Add RVM to PATH for scripting
 export PATH=/usr/local/heroku/bin:$PATH # for Heroku Toolbelt

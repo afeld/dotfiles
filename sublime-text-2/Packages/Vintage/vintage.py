e4491efa7a03cc7b294b3371f857234b93062909
Aidan Feldman <aidan.feldman@gmail.com>
2013-01-08 13:07:23 -0500

    subl: re-install packages

--- sublime-text-2/Packages/Vintage/vintage.py
+++ sublime-text-2/Packages/Vintage/vintage.py
@@ -8,7 +8,6 @@ MOTION_MODE_LINE = 2
 
 # Registers are used for clipboards and macro storage
 g_registers = {}
-REGISTER_NULL = '_'
 
 # Represents the current input state. The primary commands that interact with
 # this are:
@@ -832,11 +831,6 @@ class ViPasteLeft(ViPrefixableCommand):
                                                       'register': register})
 
 def set_register(view, register, forward):
-    if register == REGISTER_NULL:
-        # This is the null register; do nothing.
-        # More info in Vim: :help "_
-        return
-
     delta = 1
     if not forward:
         delta = -1
@@ -871,11 +865,6 @@ def set_register(view, register, forward):
             g_registers[reg] = text
 
 def get_register(view, register):
-    if register == REGISTER_NULL:
-        # This is the null register; do nothing.
-        # More info in Vim: :help "_
-        return
-
     use_sys_clipboard = view.settings().get('vintage_use_clipboard', False) == True
     register = register.lower()
     if register == '%':
@@ -1036,7 +1025,11 @@ class ViSelectBookmark(sublime_plugin.TextCommand):
     def run(self, edit, character, select_bol=False):
         self.view.run_command('select_all_bookmarks', {'name': "bookmark_" + character})
         if select_bol:
-            self.view.run_command('vi_move_to_first_non_white_space_character')
+            sels = list(self.view.sel())
+            self.view.sel().clear()
+            for r in sels:
+                start = self.view.line(r.a).begin()
+                self.view.sel().add(sublime.Region(start, start))
 
 g_macro_target = None
 

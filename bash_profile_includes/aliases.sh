c81f56febadbbd76930decf56d33daa8b1781dfc
Aidan Feldman <aidan.feldman@gmail.com>
2013-06-13 01:47:17 -0400

    remove jux-specific commands

--- bash_profile_includes/aliases.sh
+++ bash_profile_includes/aliases.sh
@@ -3,15 +3,10 @@ alias ding="afplay /System/Library/Sounds/Glass.aiff"
 alias dot="cd ~/dotfiles && subl ."
 alias ohmyzsh="subl ~/.oh-my-zsh"
 alias ss="python -m SimpleHTTPServer"
-alias tb="bundle exec torquebox run -b 0.0.0.0"
 alias zshconfig="subl ~/.zshrc"
 
 # diff full-width, c/o http://unix.stackexchange.com/a/9303
 alias diff='diff -W $(( $(tput cols) - 2 ))'
-
-# Jux-specific
-alias jux="cd ~/dev/surround/ && git pull && open surround.sublime-project"
-# alias cmp="open \"https://github.com/jux/jux/compare/deployed...`git rev-parse --symbolic-full-name --abbrev-ref HEAD`?w=1\""
 
 # PostgreSQL commands
 alias pgstart="pg_ctl -D /usr/local/var/postgres -l /usr/local/var/postgres/server.log start"

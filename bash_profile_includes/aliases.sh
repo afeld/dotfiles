d10dcf50f8a542a62db50918a511d59f5521c20e
Aidan Feldman <aidan.feldman@gmail.com>
2013-06-13 01:45:48 -0400

    remove pre-Mountain Lion color meter script

--- bash_profile_includes/aliases.sh
+++ bash_profile_includes/aliases.sh
@@ -13,9 +13,6 @@ alias diff='diff -W $(( $(tput cols) - 2 ))'
 alias jux="cd ~/dev/surround/ && git pull && open surround.sublime-project"
 # alias cmp="open \"https://github.com/jux/jux/compare/deployed...`git rev-parse --symbolic-full-name --abbrev-ref HEAD`?w=1\""
 
-# open DigitalColor Meter, displaying hex
-alias color="osascript ~/.color_meter.scpt"
-
 # PostgreSQL commands
 alias pgstart="pg_ctl -D /usr/local/var/postgres -l /usr/local/var/postgres/server.log start"
 alias pgstop="pg_ctl -D /usr/local/var/postgres stop -s -m fast"

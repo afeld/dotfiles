411d9a0694c355868c67f5e43433b08609c15787
Aidan Feldman <aidan.feldman@gmail.com>
2013-09-09 13:06:02 -0700

    remove rvm stuff

--- bash_profile_includes/rvm.sh
+++
@@ -1,6 +0,0 @@
-# export CC=/usr/bin/gcc-4.2
-
-# without this, JRuby treats 'next' as 'step' - http://kenai.com/projects/jruby/pages/UsingTheJRubyDebugger
-export JRUBY_OPTS="$JRUBY_OPTS --debug"
-
-[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" # Load RVM into a shell session *as a function*

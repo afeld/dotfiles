e6a78ca0679fc181c559c2bb16d1ebf4aafe2764
Aidan Feldman <afeld@github.com>
2014-04-22 14:10:55 -0400

    update config for new git-plugins

--- zshrc_includes/path.sh
+++ zshrc_includes/path.sh
@@ -1,4 +1,4 @@
 export PATH=$HOME/dotfiles/bin:$PATH
 export PATH=./node_modules/.bin:$PATH # add NodeJS modules from working directory
 export PATH=$PATH:$GOPATH/bin
-export PATH=$HOME/dev/git-plugins:$PATH
+export PATH=$HOME/dev/git-plugins/bin:$PATH

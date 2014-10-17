5b43cd627ef7f3993fb0ecfd8f660af19f86322b
Aidan Feldman <aidan.feldman@gmail.com>
2014-10-17 00:37:13 -0400

    include /usr/local/bin in path

--- zshrc_includes/path.sh
+++ zshrc_includes/path.sh
@@ -1,3 +1,4 @@
+export PATH=/usr/local/bin:$PATH
 export PATH=$HOME/dotfiles/bin:$PATH
 export PATH=./node_modules/.bin:$PATH # add NodeJS modules from working directory
 export PATH=$PATH:$GOPATH/bin

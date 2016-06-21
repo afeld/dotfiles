9e745bf4d570e0c95f72616e48aa57befdf47e6f
Aidan Feldman <aidan.feldman@gsa.gov>
2016-06-21 11:53:10 -0400

    use diff-highlight for git

--- zshrc_includes/path.sh
+++ zshrc_includes/path.sh
@@ -8,3 +8,6 @@ export PATH=./node_modules/.bin:$PATH # add NodeJS modules from working director
 export PATH=$PATH:$GOPATH/bin
 export PATH=$HOME/dev/git-plugins/bin:$PATH
 export PATH="$PATH:$HOME/.rvm/bin" # Add RVM to PATH for scripting
+
+# https://coderwall.com/p/nl-bdg/more-readable-git-word-diff-on-osx
+export PATH=$PATH:`brew --prefix git`/share/git-core/contrib/diff-highlight

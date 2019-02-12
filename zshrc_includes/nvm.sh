b62ecea8f003377c8a3cc7fcc55e537f6803bf72
Aidan Feldman <aidan.feldman@gmail.com>
2019-02-12 02:03:52 -0500

    lazy-load NVM to speed up shell startup

--- zshrc_includes/nvm.sh
+++ zshrc_includes/nvm.sh
@@ -1,2 +1,7 @@
 export NVM_DIR="$HOME/.nvm"
-[ -s "/usr/local/opt/nvm/nvm.sh" ] && . "/usr/local/opt/nvm/nvm.sh"
+# https://github.com/creationix/nvm/issues/1277#issuecomment-447495610
+# https://github.com/creationix/nvm/issues/539#issuecomment-245791291
+[ -s "/usr/local/opt/nvm/nvm.sh" ] && . "/usr/local/opt/nvm/nvm.sh" --no-use
+
+alias node='unalias node ; unalias npm ; nvm use default ; node $@'
+alias npm='unalias node ; unalias npm ; nvm use default ; npm $@'

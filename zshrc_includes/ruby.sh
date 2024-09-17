c800e8f1dbfc99b6c80bf81b2598c30c7fdd3547
Aidan Feldman <aidan.feldman@gmail.com>
2024-09-16 23:55:51 -0400

    no longer necessary to specify Ruby build options
    https://github.com/rbenv/ruby-build/wiki#macos

--- zshrc_includes/ruby.sh
+++
@@ -1,3 +0,0 @@
-# https://github.com/rbenv/ruby-build/wiki#openssl-version-compatibility
-OPENSSL_1_PREFIX="$(brew --prefix openssl@1.0)"
-export RUBY_CONFIGURE_OPTS="--with-openssl-dir=$OPENSSL_1_PREFIX"
